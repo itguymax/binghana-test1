05f48a3fb0a976bea4ec4f12d8bd61173ec8b423
itguymax itguymax <itguymax@gmail.com>
2019-12-07 17:05:55 +0100

    Adding comment in app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-require("dotenv").config({ debug: process.env.DEBUG });
-var createError = require('http-errors');
+require("dotenv").config({ debug: process.env.DEBUG }); // Early request for environnement varialbles config
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-const Angels = require('./src/model/Angels')
+const Angels = require('./src/model/Angels') // Contributors model
 const exphbs = require("express-handlebars");
-const db = require("./src/config/database")
+const db = require("./src/config/database") // DB config file
 
-var indexRouter = require('./src/routes/index');
-require('./src/config/paypal')
+var indexRouter = require('./src/routes/index'); 
+require('./src/config/paypal') // Paypal config file
 
 const port = process.env.PORT || 3000;
 var app = express();
 
+// DB Connection
 db.authenticate()
   .then(() => {
     console.log("Connection has been established successfully.");
@@ -20,6 +20,8 @@ db.authenticate()
   .catch(err => {
     console.error("Unable to connect to the database:", err);
   });
+
+  // Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,12 +35,17 @@ app.use(
   "/js",
   express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
 );
+// Handlebars template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("views", "./src/views");
 app.set("view engine", "handlebars");
 
+
+// api enpoint middleware
 app.use('/api', indexRouter);
 
+
+// load availble cotributor from DB on home page
 app.get('/', async (req, res)=>{
   let data;
   Angels.findAll().then(angels => {
@@ -49,6 +56,8 @@ app.get('/', async (req, res)=>{
   
   
 })
+
+// Open express server connection
 app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
